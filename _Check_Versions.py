d64d33f80927cbfb3efd88082e5a8afed4161ef6
unknown <jeffrimko@gmail.com>
2015-09-03 20:52:18 -0400

    Minor path handling update.

--- _Check_Versions.py
+++ _Check_Versions.py
@@ -2,6 +2,7 @@
 ## SECTION: Imports                                             #
 ##==============================================================#
 
+import os.path as op
 import qprompt
 from verace import VerChecker, VerInfo
 
@@ -13,23 +14,30 @@ class QpromptChecker(VerChecker):
     """Check versions in the Qprompt project."""
     NAME = "Qprompt"
     def check_setup(self):
-        path = r"lib\setup.py"
+        path = basepath(r"lib\setup.py")
         with open(path) as f:
             for num,line in enumerate(f.readlines(), 1):
                 if line.find("version =") > -1:
                     return [VerInfo(path, num, line.split('"')[1].strip())]
     def check_main(self):
-        path = r"lib\qprompt.py"
+        path = basepath(r"lib\qprompt.py")
         with open(path) as f:
             for num,line in enumerate(f.readlines(), 1):
                 if line.find("__version__ =") > -1:
                     return [VerInfo(path, num, line.split('"')[1].strip())]
     def check_log(self):
-        path = r"CHANGELOG.md"
+        path = basepath(r"CHANGELOG.md")
         with open(path) as f:
             for num,line in enumerate(f.readlines(), 1):
                 if line.find("qprompt-") > -1:
                     return [VerInfo(path, num, line.split('-')[1].split(" ")[0].strip())]
+
+##==============================================================#
+## SECTION: Function Definitions                                #
+##==============================================================#
+
+#: Returns path as absolute from base.
+basepath = lambda x: op.join(op.abspath(op.dirname(op.realpath(__file__))), x)
 
 ##==============================================================#
 ## SECTION: Main Body                                           #
