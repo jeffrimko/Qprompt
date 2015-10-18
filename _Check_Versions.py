73867b3c02a4af2c9e493817d4b2cb7159ed55d9
unknown <jeffrimko@gmail.com>
2015-10-18 15:49:18 -0400

    Updated version check script to use new release of Verace.

--- _Check_Versions.py
+++ _Check_Versions.py
@@ -2,47 +2,21 @@
 ## SECTION: Imports                                             #
 ##==============================================================#
 
-import os.path as op
-import qprompt
 from verace import VerChecker, VerInfo
 
 ##==============================================================#
-## SECTION: Class Definitions                                   #
+## SECTION: Global Definitions                                  #
 ##==============================================================#
 
-class QpromptChecker(VerChecker):
-    """Check versions in the Qprompt project."""
-    NAME = "Qprompt"
-    def check_setup(self):
-        path = basepath(r"lib\setup.py")
-        with open(path) as f:
-            for num,line in enumerate(f.readlines(), 1):
-                if line.find("version =") > -1:
-                    return [VerInfo(path, num, line.split('"')[1].strip())]
-    def check_main(self):
-        path = basepath(r"lib\qprompt.py")
-        with open(path) as f:
-            for num,line in enumerate(f.readlines(), 1):
-                if line.find("__version__ =") > -1:
-                    return [VerInfo(path, num, line.split('"')[1].strip())]
-    def check_log(self):
-        path = basepath(r"CHANGELOG.md")
-        with open(path) as f:
-            for num,line in enumerate(f.readlines(), 1):
-                if line.find("qprompt-") > -1:
-                    return [VerInfo(path, num, line.split('-')[1].split(" ")[0].strip())]
-
-##==============================================================#
-## SECTION: Function Definitions                                #
-##==============================================================#
-
-#: Returns path as absolute from base.
-basepath = lambda x: op.join(op.abspath(op.dirname(op.realpath(__file__))), x)
+VERCHK = VerChecker("Verace", __file__)
+VERCHK.include(r"lib\setup.py", opts={'match':"version = ", 'delim':'"'})
+VERCHK.include(r"lib\qprompt.py", match="__version__ = ", delim='"')
+VERCHK.include(r"CHANGELOG.adoc", match="qprompt-", delim="-", delim2=" ")
 
 ##==============================================================#
 ## SECTION: Main Body                                           #
 ##==============================================================#
 
 if __name__ == '__main__':
-    QpromptChecker().show()
-    qprompt.pause()
+    VERCHK.run()
+    raw_input("Press ENTER to continue...")
