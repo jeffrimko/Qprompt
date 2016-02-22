4afefeea8e109fce0c0c8e862bf97e900eacedc8
unknown <jeffrimko@gmail.com>
2016-02-21 20:08:56 -0500

    Updated tests to be Python 3 compatible.

--- tests/_Run_Tests.py
+++ tests/_Run_Tests.py
@@ -7,21 +7,30 @@ import subprocess
 import sys
 
 ##==============================================================#
+## SECTION: Global Definitions                                  #
+##==============================================================#
+
+_input = input if sys.version_info >= (3, 0) else raw_input
+
+##==============================================================#
 ## SECTION: Function Definitions                                #
 ##==============================================================#
 
 def run_tests():
     """Runs all found test scripts. Returns True if all tests pass."""
     fail = 0
+    okay = 0
     for i in os.listdir("."):
         if i.find("_test_") > -1 and i.endswith(".py"):
             if 0 != subprocess.call("python " + i):
                 fail += 1
-    print "[DONE]",
+            else:
+                okay += 1
+    print("[DONE]"),
     if fail:
-        print "Errors in unit tests!"
+        print("Errors in unit tests!")
         return False
-    print "All tests completely successfully!"
+    print("All %u tests completely successfully!" % (okay))
     return True
 
 ##==============================================================#
@@ -30,5 +39,5 @@ def run_tests():
 
 if __name__ == '__main__':
     okay = run_tests()
-    raw_input("Press ENTER to continue...")
+    _input("Press ENTER to continue...")
     sys.exit(0 if okay else 1)
--- tests/testlib.py
+++ tests/testlib.py
@@ -4,15 +4,26 @@
 ## SECTION: Imports                                             #
 ##==============================================================#
 
-import StringIO
 import sys
 import unittest
+
+# Handle Python 2/3 differences.
+if sys.version_info >= (3, 0):
+    from io import StringIO
+else:
+    from StringIO import StringIO
 
 ##==============================================================#
 ## SECTION: Global Definitions                                  #
 ##==============================================================#
 
+sys.path.append(r"..\lib")
+
 #{-- Allows stdin to be set via function setinput(). --
-sys.stdin = StringIO.StringIO()
-setinput = lambda x: [sys.stdin.truncate(0), sys.stdin.write(x), sys.stdin.seek(0)]
+sys.stdin = StringIO()
+setinput = lambda x: [
+        sys.stdin.seek(0),
+        sys.stdin.truncate(0),
+        sys.stdin.write(x),
+        sys.stdin.seek(0)]
 #----}
