cd8c3f0a8ca9789ce4a9f66ce8dde91febdaf10c
unknown <jeffrimko@gmail.com>
2016-04-14 22:20:33 -0400

    Bumped version number. Added test to check for fixed issue.

--- tests/menu_test_1.py
+++ tests/menu_test_1.py
@@ -15,6 +15,7 @@ from qprompt import Menu
 class TestCase(unittest.TestCase):
 
     def test_menu_1(test):
+        """Check for basic menu functionality."""
         menu = Menu()
         menu.add("1", "foo")
         menu.add("2", "bar")
@@ -30,6 +31,19 @@ class TestCase(unittest.TestCase):
         setinput("2")
         result = menu.show(ret_desc=True)
         test.assertEqual("bar", result)
+
+    def test_menu_2(test):
+        """Check for regression of fix from `0.4.1`."""
+        m1 = Menu()
+        m1.add("1", "foo")
+        m1.add("2", "bar")
+        test.assertEqual(len(m1.entries), 2)
+
+        m2 = Menu()
+        m2.add("a", "AAA")
+        m2.add("b", "BBB")
+        m2.add("c", "CCC")
+        test.assertEqual(len(m2.entries), 3)
 
 ##==============================================================#
 ## SECTION: Main Body                                           #
