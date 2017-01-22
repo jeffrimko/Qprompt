1356481565f48f01afde6f5c5e684d188907b095
unknown <jeffrimko@gmail.com>
2017-01-21 19:58:53 -0500

    Minor script updates.

--- lib/_Upload_PyPI.py
+++ lib/_Upload_PyPI.py
@@ -1,11 +1,10 @@
-"""Uploads the qprompt package to PyPI."""
+"""Uploads the package to PyPI."""
 
 ##==============================================================#
 ## SECTION: Imports                                             #
 ##==============================================================#
 
-import os
-import subprocess
+import auxly.shell as sh
 import sys
 
 import qprompt
@@ -21,11 +20,20 @@ from _Install_Package import generate_readme, cleanup_readme
 ##==============================================================#
 
 if __name__ == '__main__':
+    pause = True
+    if len(sys.argv) > 1 and "nopause" == sys.argv[1]:
+        pause = False
     ver = VERCHK.run()
     if not ver:
         qprompt.alert("Issue with version info!")
-        exit()
+        sys.exit(1)
+    if 0 != qprompt.status("Running tests...", sh.silent, [r"..\tests\_Run_Tests.py nopause"]):
+        qprompt.alert("Issue running tests!")
+        sys.exit(1)
     if qprompt.ask_yesno("Upload version `%s`?" % (ver)):
         generate_readme()
-        subprocess.call("python setup.py sdist upload", shell=True)
+        sh.call("python setup.py sdist upload")
         cleanup_readme()
+    if pause:
+        qprompt.pause()
+    sys.exit(0)
--- tests/_Run_Tests.py
+++ tests/_Run_Tests.py
@@ -38,6 +38,7 @@ def run_tests():
 
 if __name__ == '__main__':
     pause = True
+    os.chdir(os.path.dirname(__file__))
     if len(sys.argv) > 1 and "nopause" == sys.argv[1]:
         pause = False
     okay = run_tests()
