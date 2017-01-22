dca5de6b56d2b7d0551ee928a7e78306348d7312
unknown <jeffrimko@gmail.com>
2017-01-21 20:20:59 -0500

    Debugging CI issue.

--- tests/testlib.py
+++ tests/testlib.py
@@ -11,6 +11,8 @@ import unittest
 # Allows development version of library to be used instead of installed.
 libdir = op.normpath(op.join(op.abspath(op.dirname(__file__)), r"..\lib"))
 sys.path.insert(0, libdir)
+print "Libdir =", libdir
+print "SysPath =", sys.path
 
 # Handle Python 2/3 differences.
 if sys.version_info >= (3, 0):
