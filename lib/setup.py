942e32cbbcf30a9d88c01e0a24bf8042768e9cec
unknown <jeffrimko@gmail.com>
2017-01-21 19:59:10 -0500

    Updated changelog and bumped version.

--- lib/setup.py
+++ lib/setup.py
@@ -1,8 +1,9 @@
+from os.path import isfile
 from setuptools import setup, find_packages
 
 setup(
     name = "qprompt",
-    version = "0.8.0",
+    version = "0.8.1",
     author = "Jeff Rimko",
     author_email = "jeffrimko@gmail.com",
     description = "Library for quick CLI user prompts, input, and menus.",
@@ -10,7 +11,7 @@ setup(
     keywords = "cli menu prompt input user library",
     url = "https://github.com/jeffrimko/Qprompt",
     py_modules=["qprompt"],
-    long_description=open("README.rst").read(),
+    long_description=open("README.rst").read() if isfile("README.rst") else "",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Utilities",
