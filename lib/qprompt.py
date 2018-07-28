6c015a09bce5d70842d84e84544d3f084107cd8d
jeffrimko <jeffrimko@gmail.com>
2018-07-28 17:43:09 -0400

    Various Menu updates. Updated docs. Bumped version.

--- lib/qprompt.py
+++ lib/qprompt.py
@@ -32,7 +32,7 @@ else:
 ##==============================================================#
 
 #: Library version string.
-__version__ = "0.10.0"
+__version__ = "0.11.0"
 
 #: A menu entry that can call a function when selected.
 MenuEntry = namedtuple("MenuEntry", "name desc func args krgs")
