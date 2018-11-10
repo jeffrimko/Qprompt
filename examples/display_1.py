a60726f5855e035cbe2b5bfb251a91a161265bcb
jeffrimko <jeffrimko@gmail.com>
2018-11-09 20:51:30 -0500

    Minor example updates.

--- examples/display_1.py
+++ examples/display_1.py
@@ -1,6 +1,7 @@
-from qprompt import alert, echo, error, warn
+from qprompt import alert, echo, error, warn, info
 echo("Just a message.")
 alert("Heads up!")
+info("Hmm...")
 warn("Uh oh...")
 error("OMG this is bad!")
 error("REALLY BAD", end="!!!!!!!\n")
