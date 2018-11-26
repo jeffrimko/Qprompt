29a0370cb6cf5eb93cce0479f8e0161788d3a656
jeffrimko <jeffrimko@gmail.com>
2018-11-25 20:58:52 -0500

    Added demo gif. Removed example code.

--- lib/qprompt.py
+++ lib/qprompt.py
@@ -667,37 +667,4 @@ def wrap(item, args=None, krgs=None, **kwargs):
 ##==============================================================#
 
 if __name__ == '__main__':
-    teams = [
-            "Anaheim Ducks",
-            "Arizona Coyotes",
-            "Boston Bruins",
-            "Buffalo Sabres",
-            "Calgary Flames",
-            "Carolina Hurricanes",
-            "Chicago Blackhawks",
-            "Colorado Avalanche",
-            "Columbus Blue Jackets",
-            "Dallas Stars",
-            "Detroit Red Wings",
-            "Edmonton Oilers",
-            "Florida Panthers",
-            "Los Angeles Kings",
-            "Minnesota Wild",
-            "Montreal Canadiens",
-            "Nashville Predators",
-            "New Jersey Devils",
-            "New York Islanders",
-            "New York Rangers",
-            "Ottawa Senators",
-            "Philadelphia Flyers",
-            "Pittsburgh Penguins",
-            "San Jose Sharks",
-            "St. Louis Blues",
-            "Tampa Bay Lightning",
-            "Toronto Maple Leafs",
-            "Vancouver Canucks",
-            "Vegas Golden Knights",
-            "Washington Capitals",
-            "Winnipeg Jets",
-        ]
-    print(enum_menu(teams).show(returns="desc"))
+    pass
