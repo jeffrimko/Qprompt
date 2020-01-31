904e44181b9bb76e44b7c75e5cb73962d6ef9edf
jeffrimko <jeffrimko@gmail.com>
2020-01-30 20:54:40 -0500

    Bumped version and updated docs.

--- lib/qprompt.py
+++ lib/qprompt.py
@@ -59,7 +59,7 @@ def _format_kwargs(func):
 ##==============================================================#
 
 #: Library version string.
-__version__ = "0.15.4"
+__version__ = "0.15.5"
 
 #: A menu entry that can call a function when selected.
 MenuEntry = namedtuple("MenuEntry", "name desc func args krgs")
