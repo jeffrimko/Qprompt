f73fa1fad3d534ed5fbb88eef3c7d90b7884158a
jeffrimko <jeffrimko@gmail.com>
2020-04-21 17:51:12 -0400

    Added tests to check behavior when using Qprompt in a script which is called from the command line.

---
+++ tests/script_1.py
@@ -0,0 +1,19 @@
+from testlib import *
+import qprompt
+
+def _do_thing(name):
+    with open(name, "w") as fo:
+        fo.write(name)
+    print(name)
+
+def foo():
+    _do_thing("foo")
+
+def bar():
+    _do_thing("bar")
+
+def caz():
+    _do_thing("caz")
+
+menu = qprompt.Menu(foo, bar, caz)
+menu.main(default="b", loop=True)
---
+++ tests/script_test_2.py
@@ -0,0 +1,91 @@
+"""Test that arguments passed to a script main menu execute properly."""
+
+##==============================================================#
+## SECTION: Imports                                             #
+##==============================================================#
+
+from testlib import *
+
+from auxly import shell, filesys
+
+##==============================================================#
+## SECTION: Global Definitions                                  #
+##==============================================================#
+
+SCRIPT = "script_2.py"
+
+##==============================================================#
+## SECTION: Class Definitions                                   #
+##==============================================================#
+
+class TestCase(unittest.TestCase):
+
+    def _cleanup(test):
+        filesys.File("foo").delete()
+        filesys.File("bar").delete()
+        filesys.File("caz").delete()
+
+    def setUp(test):
+        test._cleanup()
+        test.assertFalse(op.exists("foo"))
+        test.assertFalse(op.exists("bar"))
+        test.assertFalse(op.exists("caz"))
+
+    def tearDown(test):
+        test._cleanup()
+
+    def test_script_1(test):
+        result = shell.call(f"python {SCRIPT} x")
+        test.assertEqual(0, result)
+        test.assertFalse(op.exists("foo"))
+        test.assertFalse(op.exists("bar"))
+        test.assertFalse(op.exists("caz"))
+
+    def test_script_2(test):
+        result = shell.call(f"python {SCRIPT} f")
+        test.assertEqual(0, result)
+        test.assertTrue(op.exists("foo"))
+        test.assertFalse(op.exists("bar"))
+        test.assertFalse(op.exists("caz"))
+
+    def test_script_3(test):
+        result = shell.call(f"python {SCRIPT} b")
+        test.assertEqual(0, result)
+        test.assertFalse(op.exists("foo"))
+        test.assertTrue(op.exists("bar"))
+        test.assertFalse(op.exists("caz"))
+
+    def test_script_4(test):
+        result = shell.call(f"python {SCRIPT} f b")
+        test.assertEqual(0, result)
+        test.assertTrue(op.exists("foo"))
+        test.assertFalse(op.exists("bar"))
+        test.assertFalse(op.exists("caz"))
+
+    def test_script_5(test):
+        result = shell.call(f"python {SCRIPT} c")
+        test.assertEqual(0, result)
+        test.assertFalse(op.exists("foo"))
+        test.assertFalse(op.exists("bar"))
+        test.assertTrue(op.exists("caz"))
+
+    def test_script_6(test):
+        result = shell.call(f"python {SCRIPT} c f")
+        test.assertEqual(0, result)
+        test.assertFalse(op.exists("foo"))
+        test.assertFalse(op.exists("bar"))
+        test.assertTrue(op.exists("caz"))
+
+    def test_script_7(test):
+        result = shell.call(f"python {SCRIPT} -d")
+        test.assertEqual(0, result)
+        test.assertFalse(op.exists("foo"))
+        test.assertTrue(op.exists("bar"))
+        test.assertFalse(op.exists("caz"))
+
+##==============================================================#
+## SECTION: Main Body                                           #
+##==============================================================#
+
+if __name__ == '__main__':
+    unittest.main()
