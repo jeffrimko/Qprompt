f78a6871574dcf471cc346963b2cae308673febb
jeffrimko <jeffrimko@gmail.com>
2020-04-21 18:09:21 -0400

    Attempt to fix travis tests.

--- tests/script_test_2.py
+++ tests/script_test_2.py
@@ -33,49 +33,49 @@ class TestCase(unittest.TestCase):
         test._cleanup()
 
     def test_script_1(test):
-        result = os.system(f"python {SCRIPT} x")
+        result = os.system("python %s x" % SCRIPT)
         test.assertEqual(0, result)
         test.assertFalse(op.exists("foo"))
         test.assertFalse(op.exists("bar"))
         test.assertFalse(op.exists("caz"))
 
     def test_script_2(test):
-        result = os.system(f"python {SCRIPT} f")
+        result = os.system("python %s f" % SCRIPT)
         test.assertEqual(0, result)
         test.assertTrue(op.exists("foo"))
         test.assertFalse(op.exists("bar"))
         test.assertFalse(op.exists("caz"))
 
     def test_script_3(test):
-        result = os.system(f"python {SCRIPT} b")
+        result = os.system("python %s b" % SCRIPT)
         test.assertEqual(0, result)
         test.assertFalse(op.exists("foo"))
         test.assertTrue(op.exists("bar"))
         test.assertFalse(op.exists("caz"))
 
     def test_script_4(test):
-        result = os.system(f"python {SCRIPT} f b")
+        result = os.system("python %s f b" % SCRIPT)
         test.assertEqual(0, result)
         test.assertTrue(op.exists("foo"))
         test.assertFalse(op.exists("bar"))
         test.assertFalse(op.exists("caz"))
 
     def test_script_5(test):
-        result = os.system(f"python {SCRIPT} c")
+        result = os.system("python %s c" % SCRIPT)
         test.assertEqual(0, result)
         test.assertFalse(op.exists("foo"))
         test.assertFalse(op.exists("bar"))
         test.assertTrue(op.exists("caz"))
 
     def test_script_6(test):
-        result = os.system(f"python {SCRIPT} c f")
+        result = os.system("python %s c f" % SCRIPT)
         test.assertEqual(0, result)
         test.assertFalse(op.exists("foo"))
         test.assertFalse(op.exists("bar"))
         test.assertTrue(op.exists("caz"))
 
     def test_script_7(test):
-        result = os.system(f"python {SCRIPT} -d")
+        result = os.system("python %s -d" % SCRIPT)
         test.assertEqual(0, result)
         test.assertFalse(op.exists("foo"))
         test.assertTrue(op.exists("bar"))
